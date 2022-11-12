788eadbdc2391d198f901755066c9bdcc3cab14f
chiragj07 <joshi.chirag54@gmail.com>
2022-11-12 15:35:09 +0530

    updated loaders

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -7,7 +7,7 @@ import {
 } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`${process.env.REACT_APP_BASE_URI}${process.env.REACT_APP_BASE_URI}/api/products/${id}`)
+  const { data } = await axios.get(`${process.env.REACT_APP_BASE_URI}/api/products/${id}`)
 
   dispatch({
     type: CART_ADD_ITEM,
